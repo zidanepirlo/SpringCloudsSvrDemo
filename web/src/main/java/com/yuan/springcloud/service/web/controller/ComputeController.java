1f309fa06645f78072b22f50c378750890b74683
yuan <zidanepirlo@163.com>
2017-11-18 21:06:39 +0800

    project init commit

---
+++ web/src/main/java/com/yuan/springcloud/service/web/controller/ComputeController.java
@@ -0,0 +1,38 @@
+package com.yuan.springcloud.service.web.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ComputeController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "fallback",
+            commandProperties = {
+            @HystrixProperty(name="execution.isolation.strategy", value="SEMAPHORE")
+    })
+    public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return r;
+    }
+
+    public  void  fallback(){
+        logger.info("fallback");
+    }
+
+}
