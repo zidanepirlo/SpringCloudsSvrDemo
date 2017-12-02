459e5b5491dbd0beddd7ed17fc70b9a6bb9b1f88
yuan <zidanepirlo@163.com>
2017-12-02 22:33:55 +0800

    modify pom for pacakge contain all jar files, add service.sh for start and stop

--- web/src/main/java/com/yuan/springcloud/service/SpringCloudSvrApp.java
+++ web/src/main/java/com/yuan/springcloud/service/SpringCloudSvrApp.java
@@ -9,10 +9,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.yuan.springcloud.service.domain.mapper.**")
-public class ComputeServiceApplication {
+public class SpringCloudSvrApp {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+		new SpringApplicationBuilder(SpringCloudSvrApp.class).web(true).run(args);
 	}
 
 }
--- web/src/main/java/com/yuan/springcloud/service/testApp.java
+++ web/src/main/java/com/yuan/springcloud/service/testApp.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ComputeServiceApplication.class)
+@SpringApplicationConfiguration(classes = SpringCloudSvrApp.class)
 @WebAppConfiguration
 public class testApp {
 
